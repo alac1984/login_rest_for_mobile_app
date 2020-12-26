db3031b43a2d12aae8236995b4aa48ab21cb460a
André Carvalho <alac1984@gmail.com>
2020-12-25 22:00:37 -0300

    Add all files

---
+++ api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+
+class UserCreateAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
+
+
